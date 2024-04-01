643081a91b4028865d4021f2fe05c12680e25d16
aemorozov <admin@aemorozov.com>
2024-04-01 20:33:04 +0400

    fix keys

--- keys.js
+++ keys.js
@@ -1,7 +1,7 @@
 const apiKeys = {
     Igor: 'w&a$-t8W0-T0Sv-3LL$',
-    Den: 'L90G-sgkN-Lg@@-!DrM ',
-    Alex: 'K*10-S4En-@dcG-VK24 '
+    Den: 'L90G-sgkN-Lg@@-!DrM',
+    Alex: 'K*10-S4En-@dcG-VK24'
 }
 
 module.exports = apiKeys;
