4a229b1146ba9779e292abb92f11f756641bafd7
aemorozov <admin@aemorozov.com>
2024-04-01 11:08:18 +0400

    test new parameters

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
-const keys = require('./keys')
+const keys = require('./keys');
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET')
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
 
 app.get('/api/keys', (req, res) => {
-    const key = req.query.key;
-    const isKeyValid = keys.includes(key)
+    const authHeader = req.headers['authorization'];
+    if (!authHeader) {
+        return res.status(401).json({ error: 'Unauthorized' });
+    }
+    const key = authHeader.split(' ')[1];
+    const isKeyValid = keys.includes(key);
     res.json({ isValid: isKeyValid });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
