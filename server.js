f9e0de739f56ddfd79c5e8cc4be586f0ae936f45
Aleksei <83358239+aemorozov@users.noreply.github.com>
2024-04-04 16:19:28 +0400

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use((req, res, next) => {
 app.get('/api/keys', (req, res) => {
     const key = req.headers['authorization'];
     if (!key) {
-        return res.status(401).json({ error: `This is privet party, go out` });
+        return res.status(401).json({ error: `demo server for license keys` });
     }
     const isKeyValid = Object.values(keys).includes(`${key}`);
     res.json(isKeyValid);
